24e23b0fcb228e536ad6ea841cb8f048118e0085
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-07-01 13:49:43 +0200

    Create ShinyModule.R
    first RShiny code to calculate a raster of migration tracks by sum(length(x)) and plot it with coastlines. interactive to choose grid size

---
+++ ShinyModule.R
@@ -0,0 +1,54 @@
+library(move)
+library(shiny)
+library(raster)
+
+shinyModuleUserInterface <- function(id, label) {
+  ns <- NS(id)
+  
+  tagList(
+    titlePanel("Raster map of migration density"),
+    sliderInput(inputId = ns("grid"), 
+                label = "Choose a raster grid size in m", 
+                value = 50000, min = 1000, max = 300000),
+    plotOutput(ns("map"))
+  )
+}
+
+shinyModuleConfiguration <- function(id, input) {
+  ns <- NS(id)
+  configuration <- list()
+
+  configuration
+}
+
+shinyModule <- function(input, output, session, data) {
+  dataObj <- reactive({ data })
+  current <- reactiveVal(data)
+  
+  migrasterObj <- reactive({
+    data.split <- split(dataObj())
+    L <- foreach(datai = data.split) %do% {
+      print(namesIndiv(datai))
+      Line(coordinates(datai))
+    }
+    names(L) <- names(data.split)
+    
+    Ls <-  Lines(L,"ID"="segm")
+    sLs <- SpatialLines(list(Ls),proj4string=CRS("+proj=longlat +ellps=WGS84"))
+    sLsT <- spTransform(sLs,CRSobj="+proj=aeqd +lat_0=53 +lon_0=24 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
+    rasterize(sLsT,raster(ext=extent(sLsT), resolution=input$grid, crs = "+proj=aeqd +lat_0=53 +lon_0=24 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs", vals=NULL),fun=function(x,...) sum(length(x)),update=TRUE)
+  })
+  
+  coastlinesObj <- reactive({
+    unzip(zipfile = "coastlines.zip", exdir = 'ne-coastlines-10m')
+    coastlines <- readOGR("ne-coastlines-10m/ne_10m_coastline.shp")
+    spTransform(coastlines,CRSobj="+proj=aeqd +lat_0=53 +lon_0=24 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
+  })
+  
+  output$map <- renderPlot({
+    plot(migrasterObj(),colNA=NA,axes=FALSE,asp=1)  
+    plot(crop(x=coastlinesObj(), y=migrasterObj()), add = TRUE)
+  })
+  
+  return(reactive({ current() }))
+}
