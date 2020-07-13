7c0948e28d4e202547b2334454bd76e649e3d117
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-07-13 10:45:15 +0200

    setwd

--- ShinyModule.R
+++ ShinyModule.R
@@ -6,6 +6,8 @@ library('sf')
 library('fasterize')
 library('rgeos')
 
+setwd("root/app/")
+
 shinyModuleUserInterface <- function(id, label, grid = 50000) {
   ns <- NS(id)
   
@@ -58,7 +60,7 @@ shinyModule <- function(input, output, session, data, grid = 50000) {
   })  
 
   coastlinesObj <- reactive({
-    coastlines <- readOGR("/root/app/ne-coastlines-10m/ne_10m_coastline.shp")
+    coastlines <- readOGR("ne-coastlines-10m/ne_10m_coastline.shp")
     coastlinesC <- crop(coastlines,extent(migrasterObj()))
     spTransform(coastlinesC,CRSobj="+proj=aeqd +lat_0=53 +lon_0=24 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
   })
